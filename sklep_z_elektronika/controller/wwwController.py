9dcfc4a6ef6856f0dc22279b32243645b58d81e1
wielorybek <63021070+wielorybek@users.noreply.github.com>
2022-01-08 00:03:04 +0100

    gotowy login

--- sklep_z_elektronika/controller/wwwController.py
+++ sklep_z_elektronika/controller/wwwController.py
@@ -3,39 +3,52 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import timedelta
 
 mc = MainController()
-app = Flask(__name__)                       # serwer Flask ktory zaimportowalem wyzej
+app = Flask(__name__)                                                   # serwer Flask ktory zaimportowalem wyzej
 app.secret_key = "p&n"
-app.permanent_session_lifetime = timedelta(hours=1)
+app.permanent_session_lifetime = timedelta(minutes=15)
 
 
-def runApp():                               # wlacznik serwera pythonowego ktory umozliwia wyswietlanie stron w przegladarce
+def runApp():                                                           # wlacznik serwera pythonowego ktory umozliwia wyswietlanie stron w przegladarce
     app.run(debug=True)
     # app.run(host="0.0.0.0")
 
 
-@app.route("/", methods=['GET', 'POST'])    # strona glowna www
-def index():                                # ekran logowania klienta
-    if request.method == "POST":            # samowywolanie lub z innej podstrony
+@app.route("/", methods=['GET', 'POST'])                                # strona glowna www
+def index():                                                            # ekran logowania klienta
+    if session.get("loggedClient") is not None:
+        return redirect(url_for("products"))
+    else:
+        return redirect(url_for("login"))                                                            # jesli strona jest wywolana wpisujac localhost:5000/ w pasek przegladarki
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":                                        # samowywolanie lub z innej podstrony
         if "login" in request.form:
-            clientID = mc.clientLogin(request.form.get("email"), request.form.get("password"))
+            clientID = mc.clientLogin(request.form.get("email-input"), request.form.get("password-input"))
             if clientID is not None:
+                print("Gitara siema!")
                 session["loggedClient"] = clientID
                 return redirect(url_for("products"))
-            # else:                         # tu jakis messageBox
-            #     print("Nie udało się zalogować!")
+            else:                                                     # tu jakis messageBox
+                print("Nie udało się zalogować!")
         elif "register" in request.form:
             return redirect(url_for("register"))
 
-    return render_template("index.html")    # jesli strona jest wywolana wpisujac localhost:5000/ w pasek przegladarki
+    return render_template("login.html")
 
 
 @app.route("/products", methods=['GET', 'POST'])
 def products():
     if request.method == "POST":
         pass
-
-    productList = None
-    return render_template("products.html", productList=productList)
+    elif request.method == "GET":
+        if session.get("loggedClient") is not None:
+            print(session.get("loggedClient"))
+            productList = None
+            return render_template("products.html", productList=productList)
+        else:
+            return redirect(url_for("login"))
 
 
 @app.route("/register", methods=['GET', 'POST'])
