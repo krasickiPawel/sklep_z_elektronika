a6c55694ba385333b5ff5b8a54fd27620fe082e5
wielorybek <63021070+wielorybek@users.noreply.github.com>
2022-01-08 02:11:14 +0100

    logowanie pracownika i klienta

--- sklep_z_elektronika/controller/wwwController.py
+++ sklep_z_elektronika/controller/wwwController.py
@@ -1,5 +1,5 @@
 from sklep_z_elektronika.controller.mainController import MainController
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, flash, render_template, request, redirect, url_for, session
 from datetime import timedelta
 
 mc = MainController()
@@ -15,40 +15,33 @@ def runApp():                                                           # wlaczn
 
 @app.route("/", methods=['GET', 'POST'])                                # strona glowna www
 def index():                                                            # ekran logowania klienta
-    if session.get("loggedClient") is not None:
-        return redirect(url_for("products"))
+    if "loggedClient" not in session:
+        return redirect(url_for("login"))
     else:
-        return redirect(url_for("login"))                                                            # jesli strona jest wywolana wpisujac localhost:5000/ w pasek przegladarki
+        if request.method == "POST":
+            pass
+
+        productList = None
+        return render_template("products.html", productList=productList)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == "POST":                                        # samowywolanie lub z innej podstrony
         if "login" in request.form:
+            print("loguje...")
             clientID = mc.clientLogin(request.form.get("email-input"), request.form.get("password-input"))
             if clientID is not None:
                 print("Gitara siema!")
                 session["loggedClient"] = clientID
-                return redirect(url_for("products"))
-            else:                                                     # tu jakis messageBox
+                return redirect(url_for("index"))
+            else:
                 print("Nie udało się zalogować!")
+                flash("Nie udało się zalogować! Wprowadź poprawny login i hasło lub przejdź do rejestracji.", "error")
         elif "register" in request.form:
             return redirect(url_for("register"))
 
-    return render_template("login.html")
-
-
-@app.route("/products", methods=['GET', 'POST'])
-def products():
-    if request.method == "POST":
-        pass
-    elif request.method == "GET":
-        if session.get("loggedClient") is not None:
-            print(session.get("loggedClient"))
-            productList = None
-            return render_template("products.html", productList=productList)
-        else:
-            return redirect(url_for("login"))
+    return render_template("login.html")                                # jesli strona jest wywolana wpisujac localhost:5000/ w pasek przegladarki
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -73,3 +66,30 @@ def basket():
         pass
 
     return render_template("basket.html")
+
+
+@app.route("/emp", methods=['GET', 'POST'])
+def emp():
+    if "loggedEmployee" not in session:
+        return redirect(url_for("empLogin"))
+    else:
+        if request.method == "POST":
+            pass
+
+        return render_template("employee.html")
+
+
+@app.route("/empLogin", methods=['GET', 'POST'])
+def empLogin():
+    if request.method == "POST":                                        # samowywolanie lub z innej podstrony
+        if "login" in request.form:
+            employeeID = mc.employeeLogin(request.form.get("email-input"), request.form.get("password-input"))
+            if employeeID is not None:
+                print("Gitara siema!")
+                session["loggedEmployee"] = employeeID
+                return redirect(url_for("emp"))
+            else:
+                print("Nie udało się zalogować!")
+                flash("Nie udało się zalogować! Wprowadź poprawny login i hasło lub przejdź do rejestracji.", "error")
+
+    return render_template("empLogin.html")                                # jesli strona jest wywolana wpisujac localhost:5000/ w pasek przegladarki
